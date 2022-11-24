30f87ba87b8ba4938977b2c6e8dc3174113b5d69
Kajetan Bucholc <58823834+LordMEZMO@users.noreply.github.com>
2022-11-24 16:43:08 +0100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,22 +1,27 @@
-console.log("Zadanie 9 na samym dole")
+console.log("Zadanie 9 na samym dole, pojawi się po 5 sec")
 new Promise(resolve => {
     setTimeout(() => console.log("Zadanie 9: Udało się!"), 5000)
 });
 
 let a = 4
 let b = 5
-const multiplysync = (x, y) => new Promise((resolve, reject) => {
+//let b = "xd"
+const multiplyAsync = (x, y) => new Promise((resolve, reject) => {
     if(typeof(x) != "number" || typeof(y) != "number"){
         reject("to nie jest liczba!")
     }
     else resolve(x*y)
 })
-console.log("Zadanie 10: "+multiplysync(a,b))
+console.log("Zadanie 10: ") 
+
+multiplyAsync(a,b)
+.then(console.log)
 
 const url = "https://jsonplaceholder.typicode.com/posts"
-console.log("Zadanie 11: \nv ")
+
 fetch(url)
 .then(res => {
+    console.log("Zadanie 11: \nv ")
     if(res.ok)
     {
         return res.json()
